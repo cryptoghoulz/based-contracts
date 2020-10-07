b8c5cfb7925863ba0ecf0cc4f3d355183243f2da
cryptoghouls <69440954+cryptoghouls@users.noreply.github.com>
2020-10-07 13:47:28 -0700

    Update IBasedGod.sol

--- contracts/v6/interface/IBasedGod.sol
+++ contracts/v6/interface/IBasedGod.sol
@@ -5,6 +5,7 @@ interface IBasedGod {
     function getSellingSchedule() external view returns (uint256);
     function weth() external view returns (address);
     function susd() external view returns (address);
+    function based() external view returns (address);
     function uniswapRouter() external view returns (address);
     function moonBase() external view returns (address);
 }
